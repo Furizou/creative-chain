1c31291405bd2aac8215888bcfc0debe783d80bd
fakhrihabb <fakhrihabb@gmail.com>
2025-10-22 07:59:35 +0700

    fix: deprecated cookieStore

--- src/app/api/creative-works/route.js
+++ src/app/api/creative-works/route.js
@@ -3,8 +3,8 @@ import { cookies } from 'next/headers'
 
 export async function GET(request) {
   try {
-    const cookieStore = cookies()
-    
+    const cookieStore = await cookies()
+
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
